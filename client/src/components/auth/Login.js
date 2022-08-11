fbfd5d64989a5470be91cd92fb9a0a00bb11c18e
vikaspuri1931 <vikaspuri1931@gmail.com>
2022-08-11 04:43:56 -0400

    Updated till 11.08.2022

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -59,79 +59,77 @@ class Login extends Component {
 
     return (
       <div>
-        
+
         <div className="background1">
-        <img src={require("../../images/pexels-francesco-ungaro-2325446.jpg")} alt="" />
-      </div>
-        <div style={{ marginTop: "4rem" }} className="row">
-       
-          <div className="col s8 offset-s2">
-            <Link to="/home" className="btn-flat waves-effect">
-              <i className="material-icons left">keyboard_backspace</i> Back to
-              home
-            </Link>
-            <div className="col s12" style={{ paddingLeft: "11.250px" }}>
-              <h4>
-                <b>Login</b> below
-              </h4>
-              <p>
-                Don't have an account? <Link to="/register">Register</Link>
-              </p>
-            </div>
-            <form noValidate onSubmit={this.onSubmit}>
-              <div className="input-field col s12">
-                
-                <label htmlFor="email">Email </label>
-                <input
-                  onChange={this.onChange}
-                  value={this.state.email}
-                  error={errors.email}
-                  id="email"
-                  type="email"
-                  className={classnames("", {
-                    invalid: errors.email || errors.emailnotfound
-                  })}
-                />
-                <span className="red-text">
-                  {errors.email}
-                  {errors.emailnotfound}
-                </span>
-              </div>
-              <div className="input-field col s12">
-                
-                <label htmlFor="password">Password</label>
-                <input
-                  onChange={this.onChange}
-                  value={this.state.password}
-                  error={errors.password}
-                  id="password"
-                  type="password"
-                  className={classnames("", {
-                    invalid: errors.password || errors.passwordincorrect
-                  })}
-                />
-                <span className="red-text">
-                  {errors.password}
-                  {errors.passwordincorrect}
-                </span>
-              </div>
-              <div className="col s12" style={{ paddingLeft: "11.250px" }}>
-                <button
-                  style={{
-                    width: "150px",
-                    borderRadius: "3px",
-                    letterSpacing: "1.5px",
-                    marginTop: "1rem"
-                  }}
-                  type="submit"
-                  className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-                >
-                  Login
-                </button>
-              </div>
-            </form>
-          </div>
+          <img src={require("../../images/pexels-frans-van-heerden-625409.jpg")} alt="" />
         </div>
+        <main>
+
+          <div class="form_content1">
+            <div class="left box">
+
+            </div>
+            <div class="right box">
+              <div style={{ marginTop: "4rem" }} className="row">
+
+                <div class="custom-link1">
+                <Link to="/home">Back to Home</Link>
+                  <div class="topic-text"><b>LOGIN</b> Below</div>
+                  <p>
+                    Don't have an account? <Link to="/register">Register</Link>
+                    </p>
+                  </div>
+                
+                  <form noValidate onSubmit={this.onSubmit}>
+                  <div class="input-box email-address">
+                                <label for="email">Email </label>
+                    
+                      <input
+                        onChange={this.onChange}
+                        value={this.state.email}
+                        error={errors.email}
+                        id="email"
+                        type="email"
+                        className={classnames("", {
+                          invalid: errors.email || errors.emailnotfound
+                        })}
+                        placeholder="Enter your email"/>
+                      <span className="red-text">
+                        {errors.email}
+                        {errors.emailnotfound}
+                      </span>
+                    </div>
+                    <div class="input-box email-address">
+                                <label for="password"> Password</label>
+                      <input
+                        onChange={this.onChange}
+                        value={this.state.password}
+                        error={errors.password}
+                        id="password"
+                        type="password"
+                        className={classnames("", {
+                          invalid: errors.password || errors.passwordincorrect
+                        })}
+                        placeholder="Enter your password"/>
+                      <span className="red-text">
+                        {errors.password}
+                        {errors.passwordincorrect}
+                      </span>
+                    </div>
+                    
+                      <button
+                        type="submit"
+                        class="btn1"
+                      >
+                        Login
+                      </button>
+                   
+                  </form>
+                
+              </div>
+            </div>
+          </div>
+        </main>
       </div>
     );
   }
--- client/src/components/layout/Contact.js
+++ client/src/components/layout/Contact.js
@@ -1,83 +1,72 @@
 import React, { Component } from "react";
 import './Contact.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPhone, faEnvelope, faLocationArrow } from '@fortawesome/free-solid-svg-icons';
 
 
 class Contact extends Component {
     render() {
         return (
             <div>
+                <div className="background3">
+                    <img src={require("../../images/pexels-daniel-ap-837932.jpg")} alt="" />
+                </div>
+
 
                 <main>
 
 
-                <div class="upper_main">
-                    <h2>Contact Us</h2>
-                    <p>If you have any Questions, Comment or Concerns</p>
-                    <p>feel free to contact us</p>
-                </div>
-                <div class="form_content">
-                    <div class="left box">
-                        <div class="address detail">
-                            <i class="fas fa-map-marker-alt"></i>
-                            <div class="topic">Address</div>
-                            <div class="text-one">Travel Tourism</div>
-                            <div class="text-two">3242, Mississauga, ON, Canada</div>
-                        </div>
-                        <div class="phone detail">
-                            <i class="fas fa-phone-alt"></i>
-                            <div class="topic">Phone</div>
-                            <div class="text-one">888-888-8888</div>
-                        </div>
-                        <div class="email detail">
-                            <i class="fas fa-envelope"></i>
-                            <div class="topic">Email</div>
-                            <div class="text-one">tour@travel.com</div>
-                        </div>
+                    <div class="upper_main">
+                        <h2>Contact Us</h2>
+                        <p>If you have any Questions, Comment or Concerns</p>
+                        <p>feel free to contact us</p>
                     </div>
-
-                    <div class="right box">
-                        <div class="topic-text">Send us a message</div>
-                        <form action="#">
-                            <div class="input-box full-name">
-                                <label for="name">Full Name</label>
-                                <input type="text" id="fullname" placeholder="Enter your name"></input>
+                    <div class="form_content">
+                        <div class="left box">
+                            <div class="address detail">
+                            <FontAwesomeIcon icon={faLocationArrow}></FontAwesomeIcon>
+                                <div class="topic">Address</div>
+                                <div class="text-one">Travelvio</div>
+                                <div class="text-two">3252 Kariya Drive, Mississauga</div>
+                                <div class="text-three">ON, Canada</div>
                             </div>
-                            <div class="input-box email-address">
-                                <label for="email">Email Address</label>
-                                <input type="text" id="emailaddress" placeholder="Enter your email"></input>
+                            <div class="phone detail">
+                            <FontAwesomeIcon icon={faPhone}></FontAwesomeIcon>
+                                <div class="topic">Phone</div>
+                                <div class="text-one">+1 888-888-8888</div>
                             </div>
-                            <div class="input-box message-box">
-                                <label for="message">Message</label>
-                                <input type="text" placeholder="Write a Message"></input>
+                            <div class="email detail">
+                            <FontAwesomeIcon icon={faEnvelope}></FontAwesomeIcon>
+                                <div class="topic">Email</div>
+                                <div class="text-one">travelvio@gmail.com</div>
                             </div>
-                            <div class="button">
-                                <input type="button" value="Send"></input>
-                            </div>
-                        </form>
-                    </div>
-
-                </div>
-
-                <div class="picture_content">
-                    <div class="left box">
-                        <div id="image1"></div>
-                    </div>
-                    <div class="middle box">
-                        <div id="image2"></div>
-                    </div>
-                    <div class="right box">
-                        <div id="image3"></div>
-                    </div>
-                </div>
-
-
-                <div class="bottom_main">
-                    <div id="bottom_image">
-                        <div>
-                            <p> “ Traveling – it leaves you speechless, then turns you into a storyteller ”</p>
                         </div>
+
+                        <div class="right box">
+                            <div class="topic-text">Send us a Message</div>
+                            <form action="#">
+                                <div class="input-box full-name">
+                                    <label for="name">Full Name</label>
+                                    <input type="text" id="fullname" placeholder="Enter your name"></input>
+                                </div>
+                                <div class="input-box email-address">
+                                    <label for="email">Email Address</label>
+                                    <input type="text" id="emailaddress" placeholder="Enter your email"></input>
+                                </div>
+                                <div class="input-box message-box">
+                                    <label for="message">Message</label>
+                                    <input type="text" placeholder="Write a Message"></input>
+                                </div>
+                                <button type="submit" class="btn2">Submit</button>
+                            </form>
+                        </div>
+
                     </div>
-                </div>
+
+
+
+
+
                 </main>
             </div>
 
