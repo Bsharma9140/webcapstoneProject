0cd00e6382ee46686c104e696c00f78ed7a9313d
Bsharma9140 <bsharma9140@conestogac.on.ca>
2022-08-12 06:59:53 -0400

    tour page

--- client/src/components/layout/Tour.js
+++ client/src/components/layout/Tour.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
 import './Tour.css';
+import info1 from "../../assets/info1.png";
+import info2 from "../../assets/info2.png";
+import info3 from "../../assets/info3.png";
+import styled from "styled-components";
 
 class Tour extends Component {
     render() {
@@ -77,45 +81,151 @@ class Tour extends Component {
         
             
           
-    <section class="iternaries">
-    <h1>Tour Packages</h1>
-    <p>We have packages through the world</p>
-        <div class="iternaries_row">
-        <div class="iternaries_col">
-            <img src={require("../../images/Dubai.jpg")} alt="" />
-   
-            <div class="layer">
-            <button type="submit" class="btn-tour">Internatinal Tour</button>
-            </div>
-            </div>
-        <div class="iternaries_col">
-            <img src={require("../../images/Iternaries_europe_2.jpg")} alt="" />
-            
-            <div class="layer">
-                <button type="submit" class="btn-tour">Domestic Tour</button>
-                
-            </div>
-            </div>
-            <div class="iternaries_col">
-                <img src={require("../../images/Usa.jpg")} alt="" />
-            
-                <div class="layer">
-                    <button type="submit" class="btn-tour">Bussiness Tour</button>
+    <Section id="recommend">
+         <h1>Tour Packages</h1>
+         <p>We have packages through the world</p>
+         <div className="destinations">
+         <div className="destination">
+              <img src={require("../../images/Dubai.jpg")} alt="" />
+              <h3>Internatinal Tour</h3>
+              <p>Singapore</p>
+              <div className="info">
+                <div className="services">
+                  <img src={info1} alt="" />
+                  <img src={info2} alt="" />
+                  <img src={info3} alt="" />
                 </div>
-                </div>
+                <h4>50,000</h4>
+         </div>
+            <div className="distance">
+            <span><button type="submit" class="btn-tour">Select</button></span>
+            </div>
         </div>
-        
-        
-    </section>
-    
-
-         </main>
-
-                
+        <div className="destination">
+              <img src={require("../../images/Iternaries_europe_2.jpg")} alt="" />
+              <h3>Internatinal Tour</h3>
+              <p>Europe, Norway, Denmark</p>
+              <div className="info">
+                <div className="services">
+                  <img src={info1} alt="" />
+                  <img src={info2} alt="" />
+                  <img src={info3} alt="" />
+                </div>
+                <h4>50,000</h4>
+         </div>
+            <div className="distance">
+            <span><button type="submit" class="btn-tour">Select</button></span>
             </div>
+        </div>
+        <div className="destination">
+              <img src={require("../../images/Usa.jpg")} alt="" />
+              <h3>Internatinal Tour</h3>
+              <p>USA,Mexico</p>
+              <div className="info">
+                <div className="services">
+                  <img src={info1} alt="" />
+                  <img src={info2} alt="" />
+                  <img src={info3} alt="" />
+                </div>
+                <h4>50,000</h4>
+         </div>
+            <div className="distance">
+            <span><button type="submit" class="btn-tour">Select</button></span>
+            </div>
+        </div>
+        </div>
+   
+    </Section>
+  </main>
+</div>
 
         );
     }
 }
+
+const Section = styled.section`
+  padding: 2rem 0;
+  .title {
+    text-align: center;
+  }
+  .packages {
+    display: flex;
+    justify-content: center;
+    margin: 2rem 0;
+    ul {
+      display: flex;
+      list-style-type: none;
+      width: max-content;
+      li {
+        padding: 1rem 2rem;
+        border-bottom: 0.1rem solid black;
+      }
+      .active {
+        border-bottom: 0.5rem solid #8338ec;
+      }
+    }
+  }
+  .destinations {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 3rem;
+    padding: 0 3rem;
+    .destination {
+      padding: 1rem;
+      display: flex;
+      flex-direction: column;
+      gap: 0.5rem;
+      background-color: #8338ec14;
+      border-radius: 1rem;
+      transition: 0.3s ease-in-out;
+      &:hover {
+        transform: translateX(0.4rem) translateY(-1rem);
+        box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+      }
+      img {
+        width: 100%;
+      }
+      .info {
+        display: flex;
+        align-items: center;
+        .services {
+          display: flex;
+          gap: 0.3rem;
+          img {
+            border-radius: 1rem;
+            background-color: #4d2ddb84;
+            width: 2rem;
+            /* padding: 1rem; */
+            padding: 0.3rem 0.4rem;
+          }
+        }
+        display: flex;
+        justify-content: space-between;
+      }
+      .distance {
+        display: flex;
+        justify-content: space-between;
+      }
+    }
+  }
+  @media screen and (min-width: 280px) and (max-width: 768px) {
+    .packages {
+      ul {
+        li {
+          padding: 0 0.5rem;
+          font-size: 2vh;
+          padding-bottom: 1rem;
+        }
+        .active {
+          border-bottom-width: 0.3rem;
+        }
+      }
+    }
+    .destinations {
+      grid-template-columns: 1fr;
+      padding: 0;
+    }
+  }
+`;
 
 export default Tour;
