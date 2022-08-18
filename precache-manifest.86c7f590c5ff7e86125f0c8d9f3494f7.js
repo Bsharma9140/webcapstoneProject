65e46e58aab0a1004ff40da115726251cae612eb
Bsharma9140 <bsharma9140@conestogac.on.ca>
2022-08-18 00:44:01 -0400

    Updates

---
+++ precache-manifest.86c7f590c5ff7e86125f0c8d9f3494f7.js
@@ -0,0 +1,270 @@
+self.__precacheManifest = [
+  {
+    "revision": "93519229f84bd2a0c7ca",
+    "url": "/webcapstoneProject/static/css/main.ac926fc7.chunk.css"
+  },
+  {
+    "revision": "93519229f84bd2a0c7ca",
+    "url": "/webcapstoneProject/static/js/main.93519229.chunk.js"
+  },
+  {
+    "revision": "6b47cd7315719ebbd59e",
+    "url": "/webcapstoneProject/static/js/1.6b47cd73.chunk.js"
+  },
+  {
+    "revision": "e420b005602b24b51b26",
+    "url": "/webcapstoneProject/static/js/runtime~main.e420b005.js"
+  },
+  {
+    "revision": "abc68bf6fe5b9d1114011b65f59bf4a6",
+    "url": "/webcapstoneProject/static/media/singapore1.abc68bf6.jpg"
+  },
+  {
+    "revision": "96769adcad3615d018dc9f907ddfb825",
+    "url": "/webcapstoneProject/static/media/maldives1.96769adc.jpg"
+  },
+  {
+    "revision": "f5689779c290599f12584c2a8d52be16",
+    "url": "/webcapstoneProject/static/media/newyork1.f5689779.jpg"
+  },
+  {
+    "revision": "c7b277ae18dea137a4a946e7d6f5ad4a",
+    "url": "/webcapstoneProject/static/media/avatarImage.c7b277ae.jpeg"
+  },
+  {
+    "revision": "ae1f929d1b4828f6288a258902230853",
+    "url": "/webcapstoneProject/static/media/logo1.ae1f929d.png"
+  },
+  {
+    "revision": "e386a02e833cf9999cf0ce30b66a04da",
+    "url": "/webcapstoneProject/static/media/maldivesN.e386a02e.jpg"
+  },
+  {
+    "revision": "d42dc053ff0bcd264a12a6554e63b702",
+    "url": "/webcapstoneProject/static/media/newyorkN.d42dc053.jpg"
+  },
+  {
+    "revision": "3a69fab2ad62be049fcc364ea3455742",
+    "url": "/webcapstoneProject/static/media/mansi.3a69fab2.jpg"
+  },
+  {
+    "revision": "330e47b285ed6f568451a600ee0a8ffb",
+    "url": "/webcapstoneProject/static/media/vikas.330e47b2.jpg"
+  },
+  {
+    "revision": "4930c0ad703171d8dc26d3f7bff62a5d",
+    "url": "/webcapstoneProject/static/media/bharti.4930c0ad.jpg"
+  },
+  {
+    "revision": "0abf019fa9c8f92d7a496cecdedf5276",
+    "url": "/webcapstoneProject/static/media/hardik.0abf019f.jpg"
+  },
+  {
+    "revision": "77c0945ba048bb11ac7e5641b0158a37",
+    "url": "/webcapstoneProject/static/media/canadaN.77c0945b.jpg"
+  },
+  {
+    "revision": "6b7a7e241501b341a703d1e9c93a8189",
+    "url": "/webcapstoneProject/static/media/newzealandN.6b7a7e24.jpg"
+  },
+  {
+    "revision": "5abdf9035034bb60d8e34430a5f388f0",
+    "url": "/webcapstoneProject/static/media/switzerlandN.5abdf903.jpg"
+  },
+  {
+    "revision": "5aafb93532b982d9283b0e1e183878d5",
+    "url": "/webcapstoneProject/static/media/amsterdamN.5aafb935.jpg"
+  },
+  {
+    "revision": "61c4cae64a86d13d025dafeca77a2944",
+    "url": "/webcapstoneProject/static/media/Destination1.61c4cae6.png"
+  },
+  {
+    "revision": "efb3cf10dfc5e1d7f9d7eb05a1d4da4f",
+    "url": "/webcapstoneProject/static/media/Destination2.efb3cf10.png"
+  },
+  {
+    "revision": "e981c3e9e6c2bb011f2fc83ada34ceba",
+    "url": "/webcapstoneProject/static/media/Destination3.e981c3e9.png"
+  },
+  {
+    "revision": "2a1099cbae1bf3da6515f2390642b619",
+    "url": "/webcapstoneProject/static/media/Destination4.2a1099cb.png"
+  },
+  {
+    "revision": "fb23f87d322cb316fd659193c18424b7",
+    "url": "/webcapstoneProject/static/media/Destination5.fb23f87d.png"
+  },
+  {
+    "revision": "61c4cae64a86d13d025dafeca77a2944",
+    "url": "/webcapstoneProject/static/media/Destination6.61c4cae6.png"
+  },
+  {
+    "revision": "75302aafb1febda530d3e87691a4c257",
+    "url": "/webcapstoneProject/static/media/pexels-bekir-donmez-3511543.75302aaf.jpg"
+  },
+  {
+    "revision": "0b461f1c09dc3acb2483f68ca9089a39",
+    "url": "/webcapstoneProject/static/media/pexels-paul-volkmer-3079710.0b461f1c.jpg"
+  },
+  {
+    "revision": "3749342a06ad3801a7b42acbca66d968",
+    "url": "/webcapstoneProject/static/media/pexels-daniel-frese-2983472.3749342a.jpg"
+  },
+  {
+    "revision": "0786c8de4eda1ad019a1e25a378bf307",
+    "url": "/webcapstoneProject/static/media/pexels-andy-vu-3283121.0786c8de.jpg"
+  },
+  {
+    "revision": "658c023cd9e566f7cfc3478fa5eb8691",
+    "url": "/webcapstoneProject/static/media/pexels-frans-van-heerden-625409.658c023c.jpg"
+  },
+  {
+    "revision": "24bed84dde45875c00d17d968346007e",
+    "url": "/webcapstoneProject/static/media/venice-6489813_1920.24bed84d.jpg"
+  },
+  {
+    "revision": "c01783e88b054beff4512fbb2f7ce791",
+    "url": "/webcapstoneProject/static/media/pexels-merlijn-vekeman-4614183.c01783e8.jpg"
+  },
+  {
+    "revision": "5c98e5aea5d09adac03ae1baba8baf78",
+    "url": "/webcapstoneProject/static/media/pexels-r-fera-2089799.5c98e5ae.jpg"
+  },
+  {
+    "revision": "639f9f32ddb45de8a175dfdbe585abaa",
+    "url": "/webcapstoneProject/static/media/copenhagen-4052654_1920.639f9f32.jpg"
+  },
+  {
+    "revision": "44bc0010bb274a80cf37f1ccbb1ee5f8",
+    "url": "/webcapstoneProject/static/media/beach-4967176_1920.44bc0010.jpg"
+  },
+  {
+    "revision": "3bbbd5a47674031214b2b566d7aae98e",
+    "url": "/webcapstoneProject/static/media/paris-4546007_1920.3bbbd5a4.jpg"
+  },
+  {
+    "revision": "8d7c46e593896f617de3a0b1abf4a2eb",
+    "url": "/webcapstoneProject/static/media/pexels-oleg-magni-1837591.8d7c46e5.jpg"
+  },
+  {
+    "revision": "64c62373b64a1f64d94eaaac7e206136",
+    "url": "/webcapstoneProject/static/media/pexels-daniel-ap-837932.64c62373.jpg"
+  },
+  {
+    "revision": "adb8ea01be24f4be3df30dd8b37a4042",
+    "url": "/webcapstoneProject/static/media/pexels-vincent-nestyapradhana-2407472.adb8ea01.jpg"
+  },
+  {
+    "revision": "4e4388bfc07e30fad34b4b6411c1a3d7",
+    "url": "/webcapstoneProject/static/media/pexels-alessandro-kelvin-2376268.4e4388bf.jpg"
+  },
+  {
+    "revision": "6738913ed06a6053e6ca305b781d3b2f",
+    "url": "/webcapstoneProject/static/media/pexels-red-zeppelin-9854596.6738913e.jpg"
+  },
+  {
+    "revision": "315f260f75d0cda0c0c9aa0bd234e50e",
+    "url": "/webcapstoneProject/static/media/pexels-arthouse-studio-4347144.315f260f.jpg"
+  },
+  {
+    "revision": "ca0103124aa2a7fb8d229c4e5d5051a8",
+    "url": "/webcapstoneProject/static/media/pexels-johannes-plenio-1146708.ca010312.jpg"
+  },
+  {
+    "revision": "a8d30a4688a5993f4b324d03dab8d98b",
+    "url": "/webcapstoneProject/static/media/pexels-pixabay-258154.a8d30a46.jpg"
+  },
+  {
+    "revision": "a3c4ece160ab43004a8e9e3664983f30",
+    "url": "/webcapstoneProject/static/media/pexels-nubia-navarro-nubikini-386009.a3c4ece1.jpg"
+  },
+  {
+    "revision": "fae051d2e778e614f04ef7bc283ef9a3",
+    "url": "/webcapstoneProject/static/media/Visas_2.fae051d2.jpg"
+  },
+  {
+    "revision": "d61b0d24cf25cdb2064984bf6f5a7d19",
+    "url": "/webcapstoneProject/static/media/Transport_2.d61b0d24.jpg"
+  },
+  {
+    "revision": "e6b6e34dd36050f3a7cb7c531e10b249",
+    "url": "/webcapstoneProject/static/media/Weather.e6b6e34d.jpg"
+  },
+  {
+    "revision": "c4175a37ef73d51bb866fef9e77b6433",
+    "url": "/webcapstoneProject/static/media/Safety.c4175a37.jpg"
+  },
+  {
+    "revision": "dece0e5935c8dfb61353696baf73243c",
+    "url": "/webcapstoneProject/static/media/History_white_1.dece0e59.jpg"
+  },
+  {
+    "revision": "c7d23600ac409d604fe80f893d1a6403",
+    "url": "/webcapstoneProject/static/media/Airport.c7d23600.jpg"
+  },
+  {
+    "revision": "4f8be468e0134450fe633b6d389fc110",
+    "url": "/webcapstoneProject/static/media/Itinerary_travel.4f8be468.jpg"
+  },
+  {
+    "revision": "ae8b97a13ebf9210ebf16286a3211625",
+    "url": "/webcapstoneProject/static/media/Festivals.ae8b97a1.jpg"
+  },
+  {
+    "revision": "b5e7132a6963f344034eb6e20486d5a3",
+    "url": "/webcapstoneProject/static/media/Etiquite.b5e7132a.jpg"
+  },
+  {
+    "revision": "88084d2c8b120e2fe88f3edbaff21485",
+    "url": "/webcapstoneProject/static/media/Payments.88084d2c.jpg"
+  },
+  {
+    "revision": "1de72899d8a5def3ddfc47c6fd93c084",
+    "url": "/webcapstoneProject/static/media/singaporeheader.1de72899.jpg"
+  },
+  {
+    "revision": "bb62a8416a8093812380922b5ad32319",
+    "url": "/webcapstoneProject/static/media/slide_big_1.bb62a841.jpg"
+  },
+  {
+    "revision": "e9b26d9b107261fdeefb5c6e34b66de8",
+    "url": "/webcapstoneProject/static/media/maldives.e9b26d9b.jpg"
+  },
+  {
+    "revision": "165030f18cd376052afbfa42994a39f1",
+    "url": "/webcapstoneProject/static/media/newyork.165030f1.jpg"
+  },
+  {
+    "revision": "7dbd58e72d6ebf462170b871d7248721",
+    "url": "/webcapstoneProject/static/media/hotel1.7dbd58e7.jpg"
+  },
+  {
+    "revision": "b6a10acceba3117fa1ee8e81451da707",
+    "url": "/webcapstoneProject/static/media/hotel2.b6a10acc.jpg"
+  },
+  {
+    "revision": "69a6fcdf15fbc572c0d4d3c388b9d857",
+    "url": "/webcapstoneProject/static/media/hotel3.69a6fcdf.jpg"
+  },
+  {
+    "revision": "bd6be825907892099ec82367f8dde2d5",
+    "url": "/webcapstoneProject/static/media/hotel4.bd6be825.jpg"
+  },
+  {
+    "revision": "ea938eb8815f49c8d881e8e7d5521ac2",
+    "url": "/webcapstoneProject/static/media/hotel5.ea938eb8.jpg"
+  },
+  {
+    "revision": "a3728a79582fb0f51b8d1eff4388c654",
+    "url": "/webcapstoneProject/static/media/checkout.a3728a79.jpg"
+  },
+  {
+    "revision": "5c8b043c51ee45e63321f7e28bf556b0",
+    "url": "/webcapstoneProject/static/media/check.5c8b043c.jpg"
+  },
+  {
+    "revision": "c06c4411fe9c4399c5484c026af13e5b",
+    "url": "/webcapstoneProject/index.html"
+  }
+];
